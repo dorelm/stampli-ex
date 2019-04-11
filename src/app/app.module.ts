7a45db62b0df2cb127a516609b1dba8ac064f9d8
Dor Elmaliach <dor.elmal11@gmail.com>
2019-04-11 08:53:52 +0300

    added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { DrawerInputComponent } from './drawer-input/drawer-input.component';
 import { FormsModule } from '@angular/forms';
 import { ElementsGridComponent } from './elements-grid/elements-grid.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ElementsGridComponent } from './elements-grid/elements-grid.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
